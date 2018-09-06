789fdda6d358f395fab600a09828f4d8309ebaef
Tyler Davis <34944514+Tylertron1998@users.noreply.github.com>
2018-09-06 13:35:55 +0100

    Update hello.js

--- test/hello.js
+++ test/hello.js
@@ -4,21 +4,19 @@ const { Node } = require('../src/index');
 
 // eslint-disable-next-line no-unused-vars
 const node = new Node('hello')
-	.on('client.connect', (client) => console.log(`[IPC] Client Connected: ${client.name}`))
-	.on('server.ready', (server) => console.log(`[IPC] Client Ready: Named ${server.name}`))
+	.on('client.connect', client => console.log(`[IPC] Client Connected: ${client.name}`))
+	.on('server.ready', server => console.log(`[IPC] Client Ready: Named ${server.name}`))
 	.on('message', message => {
 		// console.log(`Received data:`, message.data);
 		// For World.js test
 		if (message.data === 'Hello') {
 			message.reply('world!');
 		} else {
-			setTimeout(
-				() => message.reply(`Reply!: ${message.data}`),
-				Math.min(9000, Math.floor(Math.random() * 1000))
-			);
+			setTimeout(() => message.reply(`Reply!: ${message.data}`), 
+				   Math.min(9000, Math.floor(Math.random() * 1000)));
 		}
 	})
 	.on('error', (error, client) => console.error(`[IPC] Error from ${client.name}`, error))
-	.on('client.disconnect', (client) => console.log(`[IPC] Client Disconnected: ${client.name}`))
-	.on('client.destroy', (client) => console.log(`[IPC] Client Destroyed: ${client.name}`))
+	.on('client.disconnect', client => console.log(`[IPC] Client Disconnected: ${client.name}`))
+	.on('client.destroy', client => console.log(`[IPC] Client Destroyed: ${client.name}`))
 	.serve(8001);
