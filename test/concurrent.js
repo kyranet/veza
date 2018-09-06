c4d04747f9fb9967d6c15a0b7003eaa0dfe47cad
kyranet <kyradiscord@gmail.com>
2018-09-06 14:08:12 +0200

    test: Improved concurrency test

--- test/concurrent.js
+++ test/concurrent.js
@@ -3,20 +3,20 @@
 
 // This example tests concurrency with parallel messages in IPC.
 const { Node } = require('../src/index');
-const TIMES = 100;
+const TIMES = 2500;
 
 const node = new Node('concurrent')
 	.on('error', (error, client) => console.error(`[IPC] Error from ${client.name}:`, error))
 	.on('client.disconnect', (client) => console.error(`[IPC] Disconnected from ${client.name}`))
-	.on('client.ready', async (client) => {
+	.on('client.ready', (client) => {
 		console.log(`[IPC] Connected to: ${client.name}`);
-
+		console.log(`[IPC] Attempting to send and receive ${TIMES} messages...`);
 		let failed = 0, resolved = 0;
-		const promises = [];
+		let logged = false;
 		const before = Date.now();
 		for (let i = 0; i < TIMES; i++) {
-			const timeout = setTimeout(() => console.log(`Timeout reply from: ${i}`), 10000);
-			promises.push(client.send(`Test ${i}`)
+			const timeout = setTimeout(() => console.log(`Timeout reply from: ${i}`), 20000);
+			client.send(`Test ${i}`)
 				.then((reply) => {
 					clearTimeout(timeout);
 					log(`[TEST] Success: ${i} | ${reply}`);
@@ -26,17 +26,21 @@ const node = new Node('concurrent')
 					clearTimeout(timeout);
 					log(`[TEST] Failed: ${i} | ${error}`);
 					failed++;
-				}));
-		}
+				})
+				.finally(() => {
+					if (logged || failed + resolved !== TIMES) return;
+					// Show how long it took
+					console.log('[TEST]', Date.now() - before, 'milliseconds');
+					console.log('[TEST] Resolved:', resolved, 'Failed:', failed);
 
-		await Promise.all(promises);
-		console.log('[TEST]', Date.now() - before, 'milliseconds');
-		console.log('[TEST] Resolved:', resolved, 'Failed:', failed);
+					logged = true;
+				});
+		}
 	});
 
 // Connect to hello
 node.connectTo('hello', 8001)
-	.catch((error) => console.log('Disconnected!', error));
+	.catch((error) => console.error('[IPC] Disconnected!', error));
 
 // eslint-disable-next-line no-process-env
 const log = process.env.LOGS ? console.log : (arg) => arg;
