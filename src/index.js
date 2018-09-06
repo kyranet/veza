897cbac9d8bf231522cccd125fc15f371f2dc14b
kyranet <kyradiscord@gmail.com>
2018-09-07 00:10:33 +0200

    src: update exports

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-module.exports = { Node: require('./lib/Node') };
+module.exports = {
+	Base: require('./lib/Structures/Base/Base'),
+	Node: require('./lib/Node'),
+	NodeServer: require('./lib/Structures/NodeServer'),
+	NodeServerClient: require('./lib/Structures/NodeServerClient'),
+	NodeSocket: require('./lib/Structures/NodeSocket'),
+	Queue: require('./lib/Structures/Queue'),
+	SocketHandler: require('./lib/Structures/Base/SocketHandler')
+};
--- src/index.mjs
+++ src/index.mjs
@@ -1,1 +1,9 @@
-export { default as Node } from './lib/Node';
+import veza from './index.js';
+
+export const { Base } = veza;
+export const { Node } = veza;
+export const { NodeServer } = veza;
+export const { NodeServerClient } = veza;
+export const { NodeSocket } = veza;
+export const { Queue } = veza;
+export const { SocketHandler } = veza;
