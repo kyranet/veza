46c1663e42b2ee1125735e0f510bcd6788c57ef8
Tyler Davis <34944514+Tylertron1998@users.noreply.github.com>
2018-09-06 13:35:03 +0100

    Update interactive.js

--- test/interactive.js
+++ test/interactive.js
@@ -2,12 +2,13 @@
 // This Node is a socket that replies to hello.js with "world!" when it receives "Hello".
 // And uses readline to send messages to the other process.
 const { Node } = require('../src/index');
+const readline = require('readline');
 
 const node = new Node('interactive')
 	.on('error', (error, client) => console.error(`[IPC] Error from ${client.name}:`, error))
-	.on('client.disconnect', (client) => console.error(`[IPC] Disconnected from ${client.name}`))
-	.on('client.ready', (client) => console.log(`[IPC] Connected to: ${client.name}`))
-	.on('message', (message) => {
+	.on('client.disconnect', client => console.error(`[IPC] Disconnected from ${client.name}`))
+	.on('client.ready', client => console.log(`[IPC] Connected to: ${client.name}`))
+	.on('message', message => {
 		console.log(`Received data from ${message.from}:`, message);
 		if (message.data === 'Hello') {
 			message.reply('Interactive World Working!');
@@ -19,13 +20,12 @@ const node = new Node('interactive')
 node.connectTo('hello', 8001)
 	.catch(() => console.log('Disconnected!'));
 
-const readline = require('readline');
 const rl = readline.createInterface({
 	input: process.stdin,
 	output: process.stdout
 });
 
-rl.on('line', (line) => {
+rl.on('line', line => {
 	if (line) node.sendTo('hello', line, false);
 	process.stdout.write('> ');
 });
