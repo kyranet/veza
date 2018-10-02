b81e678c039d7ad8d8899d1435f74f020a7267ad
kyranet <kyradiscord@gmail.com>
2018-10-03 01:22:39 +0200

    src: Convert negative integer into positive for _compressSmallInteger

--- src/lib/Util/Transform.js
+++ src/lib/Util/Transform.js
@@ -25,14 +25,20 @@ function _packMessage(id, message, receptive = true) {
 
 // eslint-disable-next-line no-bitwise
 const MIN_32 = -~(2 ** 31), MAX_32 = 2 ** 31;
+// Checks if it is a (signed) 32-bit integer
 function _isSmallInteger(integer) {
 	return integer >= MIN_32 && integer <= MAX_32;
 }
+// Checks if it is a 8-bit integer
 function _isByteInteger(integer) {
 	return integer >= 0 && integer <= 0xFF;
 }
-function _extractByteInteger(integer) {
+// Extracts a (signed) 32-bit integer
+function _compressSmallInteger(integer) {
 	const output = [];
+	const sign = integer < 0 ? 1 : 0;
+	// If negative, convert to positive
+	if (sign) integer = -integer;
 	while (integer) {
 		// eslint-disable-next-line no-bitwise
 		output.unshift(integer & 0xFF);
@@ -40,7 +46,7 @@ function _extractByteInteger(integer) {
 		integer >>= 8;
 	}
 
-	output.unshift(integer < 0 ? 1 : 0);
+	output.unshift(sign);
 	return output;
 }
 
@@ -61,7 +67,7 @@ function _getMessageDetails(message) { // eslint-disable-line complexity
 		case 'number':
 			if (Number.isInteger(message)) {
 				if (_isByteInteger(message)) return [S_MESSAGE_TYPES.BYTE, Buffer.from(message)];
-				if (_isSmallInteger(message)) return [S_MESSAGE_TYPES.SMALL_INTEGER, Buffer.from(_extractByteInteger(message))];
+				if (_isSmallInteger(message)) return [S_MESSAGE_TYPES.SMALL_INTEGER, Buffer.from(_compressSmallInteger(message))];
 			}
 			return [S_MESSAGE_TYPES.NUMBER, Buffer.from(message.toString())];
 		case 'boolean': return [S_MESSAGE_TYPES.BOOLEAN, Buffer.from(message ? '1' : '0')];
